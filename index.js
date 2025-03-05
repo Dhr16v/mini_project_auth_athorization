3687d9952d9b7eb6f5bae607caec30fa845bf877
Dhruv gajjar <dhruvgajjar512@gmail.com>
2025-03-05 10:27:10 +0530

    c

--- index.js
+++ index.js
@@ -30,6 +30,24 @@ app.get('/logout', (req, res) => {
 app.get('/profile',isLoggedIn, async(req, res) => {
    
     let user=await userModel.findOne({email:req.user.email});
+
+    let {content}=req.body;
+    
+    postModel.create({
+        user:user._id,
+        content
+    })
+
+    user.posts.push(post._id);
+    await user.save();
+
+    res.redirect("/profile")
+
+});
+
+app.post('/post',isLoggedIn, async(req, res) => {
+   
+    let user=await userModel.findOne({email:req.user.email});
     console.log(user);
 
     res.render("profile",{user})
@@ -55,9 +73,10 @@ app.post('/register',async(req,res)=>{
             });
 
             let token=jwt.sign({email:email, userid:user._id},"shhhh");
+            console.log(token);
             res.cookie("token",token);
 
-            res.send("registerd");
+            res.redirect("/login");
         })
     })
 
@@ -84,6 +103,8 @@ app.post('/login',async(req,res)=>{
         };
     })
 })
+
+
 
 function isLoggedIn(req,res,next){
     if(req.cookies.token==="")
